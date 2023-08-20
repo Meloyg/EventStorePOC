7c33691e25ae0efafd29a5a5196c578181a77c8a
Melo Guan <meloguan123@gmail.com>
2023-08-20 17:51:24 +1200

    init

---
+++ EventStorePOC/DomainModels.cs
@@ -0,0 +1,40 @@
+﻿namespace EventStorePOC;
+
+public class BankAccount
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    private decimal CurrentBalance { get; set; }
+    private List<Transaction> Transactions = new List<Transaction>();
+
+    public BankAccount()
+    {
+    }
+
+    public void Apply(AccountCreatedEvent @event)
+    {
+        Id = @event.Id;
+        Name = @event.Name;
+        CurrentBalance = 0;
+    }
+
+    public void Apply(FundsDepositedEvent @event)
+    {
+        var newTransaction = new Transaction { Id = @event.Id, Amount = @event.Amount };
+        Transactions.Add(newTransaction);
+        CurrentBalance += @event.Amount;
+    }
+
+    public void Apply(FundsWithdrawnEvent @event)
+    {
+        var newTransaction = new Transaction { Id = @event.Id, Amount = @event.Amount };
+        Transactions.Add(newTransaction);
+        CurrentBalance -= @event.Amount;
+    }
+
+    private class Transaction
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ EventStorePOC/Events.cs
@@ -0,0 +1,42 @@
+﻿namespace EventStorePOC;
+
+public interface IEvent
+{
+    Guid Id { get; }
+}
+
+public class AccountCreatedEvent : IEvent
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+
+    public AccountCreatedEvent(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+public class FundsDepositedEvent : IEvent
+{
+    public Guid Id { get; private set; }
+    public decimal Amount { get; private set; }
+
+    public FundsDepositedEvent(Guid id, decimal amount)
+    {
+        Id = id;
+        Amount = amount;
+    }
+}
+
+public class FundsWithdrawnEvent : IEvent
+{
+    public Guid Id { get; private set; }
+    public decimal Amount { get; private set; }
+
+    public FundsWithdrawnEvent(Guid id, decimal amount)
+    {
+        Id = id;
+        Amount = amount;
+    }
+}
